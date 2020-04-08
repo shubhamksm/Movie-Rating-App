9301684a3d09fe25467147646c919a0bc4dbb087
shubhamksm <khandelwalshubham1992@gmail.com>
2020-04-09 00:50:46 +0530

    Added boiler-plate serializer and viewset for rest_framework

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer, RatingSerializer
+from .models import Movie, Rating
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
