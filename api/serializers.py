b89dc160f0591ae78140bcc98cf7dcc57ab787e8
shubhamksm <khandelwalshubham1992@gmail.com>
2020-04-09 11:32:44 +0530

    Added No. of rating and Average rating to the Movie Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description']
+        fields = ['id', 'title', 'description', 'no_of_ratings', 'avg_rating']
 
 
 class RatingSerializer(serializers.ModelSerializer):
